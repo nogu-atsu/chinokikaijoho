930d7a535e2df81b5320ea93dc273a0448a5bcfa
nogu-atsu <nogunogu11111@gmail.com>
2017-04-25 18:22:30 +0900

    一気に表示出きるようにした

--- report1.py
+++ report1.py
@@ -12,17 +12,15 @@ def train(w,b,t):
     return w_
 
 def test(w,t,p):
-    plt.figure(figsize=(1,1))
-    for j in range(len(t)):
-        x_old = t[j][:,np.newaxis]*((np.random.randint(0,100,(25,1))>(p-1))*2-1)
-        for i in range(100):
-            ind = np.random.randint(25)
-            x_new = x_old.copy()
-            x_new[ind] = (np.dot(w[ind:ind+1,:],x_old)>=0)*2-1
-            x_old = x_new.copy()
-            if i%5==0:
-                plt.imshow(x_new.reshape(5,5),"gray")
-                plt.pause(0.1)
+    x_old = t.T*((np.random.randint(0,100,(25,1))>(p-1))*2-1)
+    for i in range(100):
+        ind = np.random.randint(25)
+        x_new = x_old.copy()
+        x_new[ind] = (np.dot(w[ind:ind+1,:],x_old)>=0)*2-1
+        x_old = x_new.copy()
+        if i%5==0:
+            plt.imshow(x_new.T.reshape(-1,5),"gray")
+            plt.pause(0.1)
 if __name__=="__main__":
     t = np.array([[1,1,-1,1,1,1,-1,1,-1,1,1,-1,1,-1,1,1,-1,1,-1,1,1,1,-1,1,1],
                   [-1,1,1,1,1,-1,1,1,1,1,-1,1,1,1,1,-1,1,1,1,1,-1,1,1,1,1],
@@ -33,7 +31,7 @@ if __name__=="__main__":
     w = np.random.uniform(-1,1,(25,25))
     b = np.ones((25,1))
     w = train(w,b,t)
-    test(w,t,10)
+    test(w,t,5)
 
 
 
